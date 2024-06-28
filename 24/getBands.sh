b29b59253902f9fcc5fda51833897199dfdac6b4
nikolajwinther <nikolajwinther@gmail.com>
2024-06-28 19:00:22 +0200

    update description

--- 24/getBands.sh
+++ 24/getBands.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Script that creates a tsv-file containing band name, short description in danish, day they play, link to RF page, and link to youtube page
+# Script that scrapes the roskilde festival homepage and creates a json file for each musician
 function toline {
     sed 's/></>\n</g'
 }
@@ -14,7 +14,6 @@ dato=$(date +%Y%m%d)
 lang="da"
 curl -s https://www.roskilde-festival.dk/program/musik > "${lang}-program-$dato.html"
 bands=$(cat ${lang}-program-$dato.html | toline | grep props | xmltostring | jq .props.pageProps.modules[0].data.items | grep url | grep program | grep musik | cut -d '"' -f4 | cut -d "/" -f4)
-#bands="cristale sho-madjozi"
 for band in $bands
 do
     echo $band
